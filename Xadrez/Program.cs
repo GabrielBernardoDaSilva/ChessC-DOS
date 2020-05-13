219238c2ad44bc1a299faeace18122eb95d7a03c
Gabriel Bernardo <gabriel7798@gmail.com>
2020-05-13 15:34:11 -0300

    V0.58 clr

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -15,17 +15,17 @@ namespace Xadrez
         {
             try
             {
-                /*  Tabuleiro tab = new Tabuleiro(8, 8);
+                  Tabuleiro tab = new Tabuleiro(8, 8);
                   tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-                  tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, -3));
+     
                   tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
                   tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+                  tab.colocarPeca(new Rei(Cor.Branca, tab), new Posicao(3, 4));
+                  tab.colocarPeca(new Rei(Cor.Branca, tab), new Posicao(6, 7));
+                  tab.colocarPeca(new Rei(Cor.Branca, tab), new Posicao(1, 6));
 
-                  Tela.imprimirTabuleiro(tab);*/
+                Tela.imprimirTabuleiro(tab);
 
-                PosicaoXadrez pos = new PosicaoXadrez('c',7);
-                Console.WriteLine(pos);
-                Console.WriteLine(pos.toPosicao());
             }
             catch(TabuleiroException e)
             {
